c802753e446d788eeb53cba717ace2679e00e842
Jalvaviel <51824929+Jalvaviel@users.noreply.github.com>
2024-09-17 17:15:22 +0200

    First commit for 1.21.1

---
+++ 1.21.1/src/client/java/com/jalvaviel/MapMipMapModClient.java
@@ -0,0 +1,9 @@
+package com.jalvaviel;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MapMipMapModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+	}
+}
